d252a0424ba71571c23a216c4325de9896b00452
Neil <neil.stoker@gmail.com>
2025-12-12 11:46:39 +0000

    Add validation
    Also fixes a typo

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  validates :title, :description, :image, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
-  after_commit -> { broadcase_refresh_later_to "products" }
+  after_commit -> { broadcast_refresh_later_to "products" }
 end
