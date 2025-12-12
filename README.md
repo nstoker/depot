f28c793151b3fa815ecabab713465690092fa443
Neil <neil.stoker@gmail.com>
2025-12-12 11:51:59 +0000

    Extend validation

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Following the Agile Web Development With Rails 8 book
 Working through Task A: Creating the Application.
 
 Working through Task B: Validation and Unit Testing
+B2 Unit testing of models
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,18 @@
 class Product < ApplicationRecord
   has_one_attached :image
-  validates :title, :description, :image, presence: true
+  validates :title, uniqueness: true, presence: true
+  validates :description, :image, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validate :acceptable_image
 
   after_commit -> { broadcast_refresh_later_to "products" }
+
+  def acceptable_image
+    return unless image.attached?
+
+    acceptable_types = [ "image/gif", "image/png", "image/jpeg" ]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, "mush be a GIF, JPG, or PNG type")
+    end
+  end
 end
