f4a7f0a1ed0903e6ab8abe91367dd10cbf282eb5
Neil <neil.stoker@gmail.com>
2026-01-02 13:27:39 +0000

    Extend cart tests

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+#  Write unit tests that add both unique products and duplicate products to a cart. Assert how many products should be in the cart in each instance. Note that you'll need to modify the fixture to refer to products and carts by name -- for example, `product: pragprog`.
+fixtures :products
+
+  test "Can add a single item to a cart" do
+  end
+
+  test "Can't add duplicate items to a cart" do
+  end
 end
